fec7b1e9557c75f3baf664e65c7cc26c7f90e606
rajnish98 <rajnishpandey63@gmail.com>
2023-12-31 21:47:04 +0530

    react basic

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-  const heading = React.createElement("h1",{
-    id: "title",
-  },
-  "Heading 1"
+  const heading = React.createElement("h1", {
+      id: "title",
+    },
+    "Heading 1"
   );
-  const heading2 = React.createElement("h2",{
-    id: "title",
-  },
+  const heading2 = React.createElement("h2", {
+      id: "title",
+    },
 
-  "Heading 2"
+    "Heading 2"
 
   );
-  const container = React.createElement("div",{
-    id: "container",
-  },
+  const container = React.createElement("div", {
+      id: "container",
+    },
 
-  [heading, heading2]
+    [heading, heading2] //it is react element
   );
-  
+
   console.log(heading);
 
   const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// passing a react element inside the root
-  root.render(container);
+  // passing a react element inside the root
+  root.render(container); //render is modify of dom
